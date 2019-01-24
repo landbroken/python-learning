82f377d2e970aaebec68fbfd9ea53f40f622f93b
landbroken <445299258@qq.com>
2019-01-25 00:28:26 +0800

    update异常处理
    复习了下

--- 异常处理和测试/try_catch_finally.py
+++ 异常处理和测试/try_catch_finally.py
@@ -4,13 +4,15 @@
 # 内置异常类继承关系见https://docs.python.org/3/library/exceptions.html#exception-hierarchy
 import logging
 import time
-Logger=logging.getLogger("tryLearning")
-file_handler=logging.FileHandler("test.log")
+
+Logger = logging.getLogger("tryLearning")
+file_handler = logging.FileHandler("test.log")
 Logger.addHandler(file_handler)
 # 格式化输出
-dateNow=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-service_name = dateNow+"  tryLearning"
+dateNow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+service_name = dateNow + "  tryLearning"
 Logger.error('%s service is down!', service_name)
+
 
 def foo(a: int, b: int):
     try:
@@ -29,5 +31,8 @@ def foo(a: int, b: int):
     return ret
 
 
-v = foo(1, 0)
-print(v)
+'''
+except: division by zero
+return -1
+'''
+print(foo(1, 0))
